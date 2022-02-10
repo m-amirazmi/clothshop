6a23095912d93f79f732dc7c782e576c06797e7a
m-amirazmi <60350614+m-amirazmi@users.noreply.github.com>
2022-02-10 14:03:29 +0800

    use new url

--- src/components/HeroBanner.tsx
+++ src/components/HeroBanner.tsx
@@ -18,19 +18,23 @@ const HeroBanner: React.FunctionComponent = () => {
 
 	return (
 		<Slider {...settings}>
-			{herobanner.map((item) => (
-				<div key={item.id} className="w-100 position-relative">
-					<img src={item.image} alt={`Hero Banner ${item.id}`} className="w-100 d-none d-md-block" />
-					<img src={item.imageMob} alt={`Hero Banner ${item.id}`} className="w-100 d-md-none" />
-					<div className={styles.overlay}>
-						<small className={styles.subtitle}>{item.subtitle}</small>
-						<h2 className={styles.title}>{item.title}</h2>
-						<button onClick={() => navigate(`/shop?category=${item.category}`)} className="btn btn-accent-3 text-white px-3 py-2 rounded-0">
-							{item.cta}
-						</button>
+			{herobanner.map((item) => {
+				const imageUrl = new URL(item.image, import.meta.url).href;
+				const imageMobUrl = new URL(item.imageMob, import.meta.url).href;
+				return (
+					<div key={item.id} className="w-100 position-relative">
+						<img src={imageUrl} alt={`Hero Banner ${item.id}`} className="w-100 d-none d-md-block" />
+						<img src={imageMobUrl} alt={`Hero Banner ${item.id}`} className="w-100 d-md-none" />
+						<div className={styles.overlay}>
+							<small className={styles.subtitle}>{item.subtitle}</small>
+							<h2 className={styles.title}>{item.title}</h2>
+							<button onClick={() => navigate(`/shop?category=${item.category}`)} className="btn btn-accent-3 text-white px-3 py-2 rounded-0">
+								{item.cta}
+							</button>
+						</div>
 					</div>
-				</div>
-			))}
+				);
+			})}
 		</Slider>
 	);
 };
