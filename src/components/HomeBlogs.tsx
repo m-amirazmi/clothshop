4ebed5b5cbf97f469453b7166742243dafe60d84
m-amirazmi <60350614+m-amirazmi@users.noreply.github.com>
2022-02-12 23:40:07 +0800

    remove arrow for mobile

--- src/components/HomeBlogs.tsx
+++ src/components/HomeBlogs.tsx
@@ -12,14 +12,14 @@ const HomeBlogs: React.FunctionComponent<IBlogsProps> = ({ blogs }) => {
 		slidesToScroll: 3,
 		nextArrow: (
 			<div>
-				<div className="bg-accent-3 border-radius p-2 d-md-block rounded-3">
+				<div className="bg-accent-3 border-radius p-2 d-md-block rounded-3 d-none d-md-block">
 					<img src="/images/icons/chevron-right.svg" alt="Next" style={{ height: "24px", width: "24px" }} />
 				</div>
 			</div>
 		),
 		prevArrow: (
 			<div>
-				<div className="bg-accent-3 border-radius p-2 d-md-block rounded-3">
+				<div className="bg-accent-3 border-radius p-2 d-md-block rounded-3 d-none d-md-block">
 					<img src="/images/icons/chevron-left.svg" alt="Prev" style={{ height: "24px", width: "24px" }} />
 				</div>
 			</div>
@@ -30,6 +30,7 @@ const HomeBlogs: React.FunctionComponent<IBlogsProps> = ({ blogs }) => {
 				settings: {
 					slidesToShow: 1,
 					slidesToScroll: 1,
+					centerMode: true,
 				},
 			},
 		],
@@ -37,14 +38,14 @@ const HomeBlogs: React.FunctionComponent<IBlogsProps> = ({ blogs }) => {
 	return (
 		<div className="mb-5" style={{ padding: "36px 0px", background: "#FFF3F3" }}>
 			<HomeSectionHeader title="Latest News">
-				<Slider {...settings}>
+				<Slider {...settings} className="overflow-hidden mb-5">
 					{blogs.map((item) => (
 						<div key={item.id} className="px-2">
 							<Card body className="border-0 rounded-3">
 								<img src={item.image} alt={item.title} className="rounded-3" />
 								<div className="mt-3">
 									<small style={{ color: "rgba(0,0,0,0.6)" }}>{new Date(item.date).toLocaleDateString("en-MY", { year: "numeric", month: "long", day: "numeric" })}</small>
-									<p className="mt-2">{item.title.substring(0, 60)}...</p>
+									<p className="mt-2">{item.title.substring(0, 50)}...</p>
 									<Link to={item.ctaLink} className="text-accent-3" style={{ fontSize: "14px" }}>
 										Read More
 									</Link>
