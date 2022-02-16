50ed0616551433608580ccf5d1b1e15e492ccb74
m-amirazmi <60350614+m-amirazmi@users.noreply.github.com>
2022-02-16 10:28:54 +0800

    start redux for filter

---
+++ src/redux/features/plpFilterSlice.ts
@@ -0,0 +1,12 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+const initialState = {};
+
+export const plpFilterSlice = createSlice({
+	name: "filter",
+	initialState,
+	reducers: {},
+});
+
+export const {} = plpFilterSlice.actions;
+export default plpFilterSlice.reducer;
